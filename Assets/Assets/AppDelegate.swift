e4fde616c6e4d72c574a9ca48ea7f314056fa012
Arnaud Thiercelin <athiercelin@icloud.com>
2016-08-05 22:01:22 -0700

    Initial commit.

---
+++ Assets/Assets/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Assets
+//
+//  Created by Arnaud Thiercelin on 8/5/16.
+//  Copyright © 2016 Arnaud Thiercelin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Assets/Assets/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Assets
+//
+//  Created by Arnaud Thiercelin on 8/5/16.
+//  Copyright © 2016 Arnaud Thiercelin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
